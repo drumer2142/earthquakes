6acf728f91137136f348649e9c0fbdba969850b6
drumer2142 <fotis360@windowslive.com>
2023-01-26 21:39:06 +0200

    Add distance test

--- handlers/feed_handler_test.go
+++ handlers/feed_handler_test.go
@@ -1,6 +1,9 @@
 package handlers
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 var testURLs = []string{
 	"http://www.geophysics.geol.uoa.gr/stations/maps/seismicity.xml",
@@ -15,5 +18,17 @@ func TestFetchSingleFeed(t *testing.T) {
 		feedChan := make(chan feedResult, len(testURLs))
 		fetchSingleFeed(url, feedChan)
 	}
+}
 
+func TestFetchFeedWithTicker(t *testing.T) {
+	for i := 0; i < 4; i++ {
+		FetchFeeds(testURLs)
+	}
+}
+
+func TestDistance(t *testing.T) {
+	lat := 35.90
+	long := 28.13
+	quakeDistanceInKM := distance(FixedLatitude, FixedLongitude, lat, long, "K")
+	assert.Equal(t, 457.05984795877754, quakeDistanceInKM)
 }
